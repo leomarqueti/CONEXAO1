af5515d93a192fc91924d754cdb6c8073bcccd7a
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2022-12-13 20:34:47 -0300

    Add files via upload

---
+++ envio.js
@@ -0,0 +1,24 @@
+// Primeira página
+
+var nome = document.getElementById("inputNome").value; // pego o valor do input de nome
+
+var email = document.getElementById("inputEmail").value; // pego o valor do input de email
+
+// Armazeno as informações em um objeto
+var dadosUsuario = {
+    nome: nome,
+    email: email
+};
+
+
+function pegarDados(){
+    // Iniciar a conexão WebSocket
+    const socket = new WebSocket('https://leomarqueti.github.io/CONEXAO2/');
+
+    // Enviar dados quando a conexão for estabelecida
+    socket.onopen = function() {
+    socket.send('Nome do cliente: João da Silva');
+    socket.send('Veículo: Ford Fusion');
+    };
+
+}
