8ddc0a9b122d2b81c74514e7ddfcb38bba2532db
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2022-12-13 23:18:08 -0300

    Add files via upload

--- envio.js
+++ envio.js
@@ -1,17 +1,15 @@
 // Primeira página
 
-var nome = document.getElementById("inputNome").value; // pego o valor do input de nome
-
-var email = document.getElementById("inputEmail").value; // pego o valor do input de email
-
-// Armazeno as informações em um objeto
-var dadosUsuario = {
-    nome: "leo",
-    email: email
-};
-
-
 function pegarDados(){
+    var nome = document.getElementById("inputNome").value; // pego o valor do input de nome
+    var email = document.getElementById("inputEmail").value; // pego o valor do input de email
+
+    // Armazeno as informações em um objeto
+    var dadosUsuario = {
+        nome: nome,
+        email: email
+    };
+
     // Na primeira página, crie um canal de broadcast e envie uma mensagem
     const broadcastChannel = new BroadcastChannel('canalleo');
     broadcastChannel.postMessage(dadosUsuario);
