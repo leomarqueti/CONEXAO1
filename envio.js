77110c26d9b60a791c783f942672b692748a4837
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2022-12-13 22:39:25 -0300

    Update envio.js

--- envio.js
+++ envio.js
@@ -1,24 +1,3 @@
-// Primeira página
-
-var nome = document.getElementById("inputNome").value; // pego o valor do input de nome
-
-var email = document.getElementById("inputEmail").value; // pego o valor do input de email
-
-// Armazeno as informações em um objeto
-var dadosUsuario = {
-    nome: nome,
-    email: email
-};
-
-
-function pegarDados(){
-    // Iniciar a conexão WebSocket
-    const socket = new WebSocket('wss://leomarqueti.github.io/CONEXAO2/');
-
-    // Enviar dados quando a conexão for estabelecida
-    socket.onopen = function() {
-    socket.send('Nome do cliente: João da Silva');
-    socket.send('Veículo: Ford Fusion');
-    };
-
-}
+// Na primeira página, crie um canal de broadcast e envie uma mensagem
+const broadcastChannel = new BroadcastChannel('canalleo');
+broadcastChannel.postMessage('deu certo porra');
